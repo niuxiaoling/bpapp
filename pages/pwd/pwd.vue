3b8990eb17ee34fee93770f6a44647dc1e666efe
niuxiaoling <17607190392@163.com>
2018-10-28 11:15:34 +0800

    first commit

---
+++ pages/pwd/pwd.vue
@@ -0,0 +1,11 @@
+<template>
+	<view>
+		忘记密码
+	</view>
+</template>
+<script>
+	
+</script>
+<style>
+	
+</style>
---
+++ pages/reg/reg.vue
@@ -0,0 +1,11 @@
+<template>
+	<view>
+		注册账号
+	</view>
+</template>
+<script>
+	
+</script>
+<style>
+	
+</style>
