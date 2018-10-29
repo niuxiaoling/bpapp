fcd00f4976f27ba68924116e585464e5ee8433e8
niuxiaoling <17607190392@163.com>
2018-10-29 22:30:31 +0800

    two page

---
+++ pages/PersonalCenter/PersonalCenter.vue
@@ -0,0 +1,174 @@
+<template>
+	<view class="avatars">
+		<view class="avatar_box">
+			<view class="avatar">
+				<image src="../../static/img/tx_rbt.jpg" mode="aspectFit"></image>
+				<view class="sign">
+					<image src="../../static/img/viptest.jpg" mode="aspectFit"></image>
+				</view>
+			</view>
+		</view>
+		<text class="phoneNo">18888888888</text>
+		<view class="income">
+			<view class="income-list">
+				<text class="list-No">12</text>
+				<rich-text class="list-text">总收入</rich-text>
+				
+			</view>
+			<view class="income-list">
+				<text class="list-No">12</text>
+				<rich-text class="list-text">今日收入</rich-text>
+				
+			</view>
+			<view class="income-list">
+				<text class="list-No">12</text>
+				<rich-text class="list-text">已提现</rich-text>
+			</view>
+		</view>
+		<view class="lists">
+			<view class="listli">
+				 <image src="../../static/img/GRZL.png" mode="aspectFit"></image><text>个人资料</text>
+			</view>
+			
+			<view class="listli">
+				<image src="../../static/img/VIP.png" mode="aspectFit"></image><text>账号会员</text>
+				</view>
+			<view class="listli">
+				<image src="../../static/img/TD.png" mode="aspectFit"></image><text>账号会员</text>
+			</view>
+			<view class="listli">
+				<image src="../../static/img/WDXZ.png" mode="aspectFit"></image><text>我的献值</text>
+				</view>
+			<view class="listli">
+				<image src="../../static/img/QB.png" mode="aspectFit"></image><text>我的钱包</text>
+			</view>
+			<view class="listli">
+				<image src="../../static/img/YHK.png" mode="aspectFit"></image><text>绑定银行卡</text>
+			</view>
+			<view class="listli">
+				<image src="../../static/img/TX.png" mode="aspectFit"></image><text>提现</text>
+			</view>
+			<view class="listli">
+				<image src="../../static/img/JCGX.png" mode="aspectFit"></image><text>检查更新</text>
+			</view>
+			<view class="listli">
+				<image src="../../static/img/QHZH.png" mode="aspectFit"></image><text>切换账号</text>
+			</view>
+	
+		</view>
+		<view><button class="getOut">退出APP</button>
+		</view>
+		</view>
+		
+</template>
+
+<script>
+	export default {
+		data:{
+			
+		},
+		methods:{
+			
+		}
+		
+	}
+</script>
+
+<style>
+	.avatars{
+		background:linear-gradient(bottom, #E61532,#E86318);
+		padding-top:50upx;
+	}
+	.avatar_box{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		padding:10upx 0;
+	}
+	.avatar{
+		position:relative;
+		width: 120upx;
+		height: 120upx;
+		border-redius:50%;
+		
+	}
+	.avatar image{
+		width: 120upx;
+		height: 120upx;
+		border-radius:50%;
+	}
+	.avatar-msg{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 40upx;
+	}
+	.avatar-msg view{
+		
+		margin: 0 10upx;
+	}
+	.sign{
+		position:absolute;
+		bottom:0upx;
+		left:50upx;
+		width: 130upx;
+	}
+	.sign image{
+		width: 130upx;
+		height: 30upx;
+	}
+	.phoneNo{
+		color:#fff;
+		display: flex;
+		justify-content: center;
+		margin-top:10upx;
+		padding-bottom:20upx;
+	}
+	.income{
+		display: flex;
+		flex-wrap:nowrap;
+		border-top:1px solid #FFD39C;	
+		
+	}
+	.income-list{
+		width:33.3%;
+		display:flex;
+		flex-direction:column;
+		justify-content:center;
+		align-items:center;
+		padding-top:20upx;
+		color:#FFF;
+	}
+	.list-text{
+		margin-bottom:20upx
+	}
+	.lists{
+		background-color: #F4F0F1;
+	}
+	
+	.lists .listli{
+		width: 100%;
+		height: 100upx;
+		align-items:center;
+		display: flex;
+		margin: 10upx 0;
+		/* padding-left:5%; */
+		border-bottom:1px solid #dadada;
+		
+	}
+	.lists .listli image{
+		width: 60upx;
+		height: 60upx;
+		margin-left:20upx;
+		
+	}
+	.lists .listli text{
+		margin-left:10upx;
+	}
+	.getOut{
+		background:linear-gradient(bottom, #E61532,#E86318);
+		color:#FFF;
+	}
+</style>
--- pages/main/main.vue
+++ pages/main/main.vue
@@ -3,36 +3,23 @@
 		<view class="swiperbox">
 			<swiper :indicator-dots="indicatorDots" indicator-active-color="#3AC198" :autoplay="autoplay" :interval="interval" :duration="duration">
 				<swiper-item v-for="item in itemList" :key="item">
-					<text>{{item}}</text>
+					<image :src = "item"  mode="scaleToFill"></image>
 				</swiper-item>
 			</swiper>
 		</view>
-		<view class="banner">
-			<swiper :indicator-dots="false" :autoplay="autoplay" :interval="interval" :duration="duration" :vertical="true">
-				<swiper-item v-for="item in itemList2" :key="item">
-					<text>{{item}}</text>
-				</swiper-item>
-			</swiper>
+		<view class="banner marquee_box">
+			<text class="marquee_text" >
+				床前明月光，疑是地上霜。	
+			</text>
+			  
 		</view>
 		<view class="list">
-			<view class="list_item">
-				<image src="../../static/img/book.png" mode="scaleToFill"></image>
-				<text>如何赚佣</text>
+			<view class="list_item" v-for="v in list" :key="v">
+				<navigator  :url="v.url">
+					<image :src="v.imgage" mode="scaleToFill"></image>
+					<text>{{v.text}}</text>
+				</navigator >
 			</view>
-			<view class="list_item">
-				<image src="../../static/img/book.png" mode="scaleToFill"></image>
-				<text>如何赚佣</text>
-			</view>
-			<view class="list_item">
-				<image src="../../static/img/book.png" mode="scaleToFill"></image>
-				<text>如何赚佣</text>
-			</view>
-			<view class="list_item">
-				<image src="../../static/img/book.png" mode="scaleToFill"></image>
-				<text>如何赚佣</text>
-			</view>
-			<view></view>
-			<view></view>
 		</view>
 	</view>
 </template>
@@ -40,22 +27,61 @@
 	export default{
 		data:{
 			itemList: [
-				'item1',
-				'item2',
-				'item3'
+				'../../static/img/banner1.jpg',
+				'../../static/img/banner2.jpg'
 			],
-			itemList2: [
-				'item1',
-				'item2',
-				'item3'
+			list:[
+				{
+					imgage:'../../static/img/book.png',
+					text:'如何赚佣',
+					url:'/pages/pwd/pwd'
+				},
+				{
+					imgage:'../../static/img/book.png',
+					text:'推广二维码',
+					url:"/pages/pwd/pwd"
+				},
+				{
+					imgage:'../../static/img/book.png',
+					text:'广告投放',
+					url:'/pages/pwd/pwd'
+				},
+				{
+					imgage:'../../static/img/book.png',
+					text:'会员',
+					url:'/pages/pwd/pwd'
+				}
 			],
 			indicatorDots: true,
 			autoplay: true,
 			interval: 5000,
-			duration: 1000
+			duration: 1000,
+			orientation:'left',
+			text:'这是一条会滚动的文字滚来滚去的文字跑马灯，哈哈哈哈哈哈哈哈',
+			marqueePace: 1,//滚动速度
+		    marqueeDistance: 0,//初始滚动距离
+		    marqueeDistance2: 0,
+		    marquee2copy_status: false,
+		    marquee2_margin: 60,
+		    size: 14,
+
 		},
 		methods: {
-		}
+			 
+		},
+		mounted(){
+		},
+// 		onShow() {
+// 			var vm = this;
+// 			var length = vm.data.text.length * vm.data.size;//文字长度
+// 			var windowWidth = uni.getSystemInfoSync().windowWidth;// 屏幕宽度
+// 			 vm.setData({
+// 				length: length,
+// 				windowWidth: windowWidth,
+// 				marquee2_margin: length < windowWidth ? windowWidth - length : vm.data.marquee2_margin//当文字长度小于屏幕长度时，需要增加补白
+// 
+//     		});
+// 		}
 	}
 </script>
 <style>
@@ -69,6 +95,9 @@
 		background:#EDEDED;
 		line-height:100upx;
 	}
+	.marquee_box{
+		position:relative;
+	}
 	.list{
 		display:flex;
 		flex-wrap:wrap;
@@ -81,6 +110,12 @@
 		justify-content:center;
 		align-items:center;
 		padding-top:12upx;
+	}
+	.list .list_item navigator{
+		display:flex;
+		flex-direction:column;
+		justify-content:center;
+		align-items:center;
 	}
 	.list_item image{
 		width:80upx;
