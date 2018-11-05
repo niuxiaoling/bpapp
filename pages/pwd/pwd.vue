c4b998263223813d3a4edc326d6997253d611f8d
niuxiaoling <17607190392@163.com>
2018-11-05 22:48:00 +0800

    jiekou

--- pages/pwd/pwd.vue
+++ pages/pwd/pwd.vue
@@ -5,29 +5,92 @@
 		</view>
 		<view class="reg-list">
 			<image src="../../static/img/shouji.png"></image>
-			<input type="text" v-model="phoneNo" placeholder="手机号" maxlength="11" placeholder-style="color:#ffffff"/>
+			<input type="text" v-model="account" placeholder="账号" maxlength="11" placeholder-style="color:#ffffff"/>
 		</view>
 		<view class="reg-list">
 			<image src="../../static/img/miam.png"></image>
-			<input type="text" password="true" v-model="phoneNo" placeholder="请输入新密码"  placeholder-style="color:#ffffff"/>
+			<input type="text" password="true" v-model="password" placeholder="请输入新密码"  placeholder-style="color:#ffffff"/>
 		</view>
 		<view class="reg-list">
 			<image src="../../static/img/querenmima.png"></image>
-			<input type="text" password="true" v-model="phoneNo" placeholder="请确认密码" placeholder-style="color:#ffffff"/>
+			<input type="text" password="true" v-model="newPassword" placeholder="请确认密码" placeholder-style="color:#ffffff"/>
 		</view>
-		<view class="reg-list">
+		<!-- <view class="reg-list">
 			<image src="../../static/img/yanzhengma.png"></image>
 			<input type="text" v-model="phoneNo" placeholder="短信验证码" placeholder-style="color:#ffffff"/>
 			<button><text>|</text>获取</button>
-		</view>
+		</view> -->
 		
 		<view class="regBut">
-			<button> 确 定</button>
+			<button @tap="submit"> 确 定</button>
 		</view>
 	</view>
 </template>
 <script>
-	
+	export default{
+		data(){
+			return{
+				account:'',
+				password:'',
+				newPassword:""
+			}
+		},
+		methods:{
+		// 提交
+			submit(){
+				if (this.account.length < 5) {
+					uni.showToast({
+						icon: 'none',
+						title: '账号最短为 5 个字符'
+					});
+					return;
+				}
+				if (this.password.length < 6) {
+					uni.showToast({
+						icon: 'none',
+						title: '密码最短为 6 个字符'
+					});
+					return;
+				}
+				if(this.password != this.newPassword){
+					uni.showToast({
+						icon: 'none',
+						title: '两次密码不一致'
+					});
+					return;
+				}
+				const data ={
+					password:this.phone,
+					account:this.account,
+					newPassword:this.newPassword,
+				}
+				uni.request({
+					url:'http://10.138.93.113:2221/ScreenTheWord/MainController.do',
+					method:'POST',
+					header: {
+						'Access-Control-Allow-Origin':'*' 
+					},
+					data:{
+						controllerRequestType:'loginControllerService',
+						jsonString:{
+							'requestType':"updateUserInfo",
+							userInfo:data
+						}	
+					},
+					
+					success: (res) => {
+						// if();
+						uni.showToast({
+							title: '修改成功'
+						});
+					},
+					fail:(response) =>{
+						
+					}
+				})
+			}
+		}
+	}
 </script>
 <style>
 	@import '../../common/denglu.css';
--- pages/reg/reg.vue
+++ pages/reg/reg.vue
@@ -5,32 +5,103 @@
 		</view>
 		<view class="reg-list">
 			<image src="../../static/img/shouji.png"></image>
-			<input type="text" v-model="phoneNo" placeholder="手机号" maxlength="11" placeholder-style="color:#ffffff"/>
+			<input type="text" v-model="phone" placeholder="手机号" maxlength="11" placeholder-style="color:#ffffff"/>
 		</view>
 		<view class="reg-list">
 			<image src="../../static/img/yanzhengma.png"></image>
-			<input type="text" v-model="phoneNo" placeholder="短信验证码" placeholder-style="color:#ffffff"/>
-			<button><text>|</text>获取</button>
+			<input type="text" v-model="verificationCode" placeholder="手机验证码" placeholder-style="color:#ffffff"/>
+			<button @tap="getPhone"><text>|</text>获取</button>
+		</view>
+		<view class="reg-list">
+			<image src="../../static/img/yanzhengma.png"></image>
+			<input type="text" v-model="account" placeholder="用户名" placeholder-style="color:#ffffff"/>
 		</view>
 		<view class="reg-list">
 			<image src="../../static/img/miam.png"></image>
-			<input type="text" v-model="phoneNo" placeholder="密码"  placeholder-style="color:#ffffff"/>
-		</view>
-		<view class="reg-list">
-			<image src="../../static/img/querenmima.png"></image>
-			<input type="text" v-model="phoneNo" placeholder="确认密码" placeholder-style="color:#ffffff"/>
+			<input type="text" v-model="password" placeholder="密码"  placeholder-style="color:#ffffff"/>
 		</view>
 		<view class="reg-list">
 			<image src="../../static/img/yaoqingma.png"></image>
-			<input type="text" v-model="phoneNo" placeholder="邀请码" placeholder-style="color:#ffffff"/>
+			<input type="text" v-model="invitationCode" placeholder="邀请码" placeholder-style="color:#ffffff"/>
 		</view>
 		<view class="regBut">
-			<button>确认注册</button>
+			<button @tap="register">确认注册</button>
 		</view>
 	</view>
 </template>
 <script>
-	
+	export default{
+		data(){
+			return{
+				phone:'',
+				verificationCode:'',
+				account:"",
+				password:'',
+				invitationCode:''
+			}
+		},
+		methods:{
+		// 获取手机验证码
+			getPhone(){
+				//缺少接口
+			},
+		// 注册
+			register(){
+				if (this.account.length < 5) {
+					uni.showToast({
+						icon: 'none',
+						title: '账号最短为 5 个字符'
+					});
+					return;
+				}
+				if (this.password.length < 6) {
+					uni.showToast({
+						icon: 'none',
+						title: '密码最短为 6 个字符'
+					});
+					return;
+				}
+				if(this.verificationCode ==''){
+					uni.showToast({
+						icon: 'none',
+						title: '请输入短信验证码'
+					});
+					return;
+				}
+				const data ={
+					phone:this.phone,
+					verificationCode:this.verificationCode,
+					account:this.account,
+					password:this.password,
+					invitationCode:this.invitationCode
+				}
+				uni.request({
+					url:'http://10.138.93.113:2221/ScreenTheWord/MainController.do',
+					method:'POST',
+					header: {
+						'Access-Control-Allow-Origin':'*' 
+					},
+					data:{
+						controllerRequestType:'loginControllerService',
+						jsonString:{
+							'requestType':"registUserInfo",
+							 userInfo:data
+						}	
+					},
+					
+					success: (res) => {
+						// if();
+						uni.showToast({
+							title: '注册成功'
+						});
+					},
+					fail:(response) =>{
+						
+					}
+				})
+			}
+		}
+	}
 </script>
 <style>
 	@import '../../common/denglu.css';
