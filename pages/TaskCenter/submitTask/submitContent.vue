f3d1b60be561a2aefa68463b8acaf07e4a5fd586
niuxiaoling <17607190392@163.com>
2018-12-07 22:29:38 +0800

    submit

--- pages/TaskCenter/submitTask/submitContent.vue
+++ pages/TaskCenter/submitTask/submitContent.vue
@@ -11,7 +11,7 @@
 			<view class="btn-upload" @tap="chooseImg">请选择图片</view>
 		</view>
 		<view class="imgbox">
-			<image :src="uploadimg" mode="aspectFit"  class="imgbox-img"></image>
+			<image :src="uploadimg" mode="aspectFit"  class="imgbox-img" @tap="previewImage" @longpress="removeImage(image)"></image>
 		</view>
 		<view v-if="uploadimg !=''">
 			<button class="getTasks"  @tap="getTask">提交</button>
@@ -47,94 +47,79 @@
 		},
 		onLoad(options){
 			 let order = JSON.parse(options.task);
-			 console.log(order.taskInfo);
 			 this.tasksDetails.taskTitle = order.taskInfo.taskTitle;
 			 this.orderInfo.taskIdentifier = order.taskInfo.taskIdentifier; //编号
 			 this.orderInfo.account = uni.getStorageSync('userInfo').account;
 		},
 		methods:{
 			
-			chooseImg(){
+			chooseImg() {
 				var that = this;
+				//选择图片
 				uni.chooseImage({
-					count: 1, //默认9
-					sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
-					sourceType: ['album'], //从相册选择
-					success: function (res) {
-						uni.showLoading({
-							mask:true,
-							title:'loading'
-						})
+					sourceType: ['camera', 'album'],
+					sizeType: 'compressed',
+					count: 1,
+					success: function(res) { 
 						uni.request({
-						url:res.tempFilePaths[0],
-						method:'GET',
-						responseType: 'arraybuffer',
-						success: ress => {
-							var base64 = wx.arrayBufferToBase64(ress.data); //把arraybuffer转成base64 
-							// base64 = 'data:image/jpeg;base64,' + base64 //不加上这串字符，在页面无法显示的哦
-							// that.base64 = base64;
-							const jsonString = {
-								taskInfo:{
-									picBast64:base64,
-									upType:'pic',
-								},
-								requestType:"upFile",
-							}
-							const param ={
-								controllerRequestType:"taskControllerService",
-								jsonString:JSON.stringify(jsonString)
-							}
-							uni.request({
-								url:that.websiteUrl,
-								method:'POST',
-								header: {
-									"content-type":"application/x-www-form-urlencoded"
-								},
-								data:param,
-								success: (dat) => {
-									if(dat.data.errorCode == '0000'){
-							
-										console.log(dat.data)
-										uni.getImageInfo({
-											src: res.tempFilePaths[0],
-											success: function (image) {
-												uni.hideLoading();
-												// console.log(image);
-												that.uploadimg = image.path;
-											}
-										});
-									}
-								},
-								fail:(dat) =>{
-									uni.hideLoading();
-									console.log(JSON.stringify(res));
+							url:res.tempFilePaths[0],
+							method:'GET',
+							responseType: 'arraybuffer',
+							success: ress => {
+								var base64 = wx.arrayBufferToBase64(ress.data); //把arraybuffer转成base64 
+								// base64 = 'data:image/jpeg;base64,' + base64 //不加上这串字符，在页面无法显示的哦
+								// that.base64 = base64;
+								const jsonString = {
+									taskInfo:{
+										picBast64:base64,
+										upType:'pic',
+									},
+									requestType:"upFile",
 								}
-							})
-							
-						}
+								const param ={
+									controllerRequestType:"taskControllerService",
+									jsonString:JSON.stringify(jsonString)
+								}
+								uni.request({
+									url:that.websiteUrl,
+									method:'POST',
+									header: {
+										"content-type":"application/x-www-form-urlencoded"
+									},
+									data:param,
+									success: (res) => {
+										if(res.data.errorCode == '0000'){
+											that.picId.push(res.data.taskInfo.picId);
+										}
+									},
+									fail:(res) =>{
+										console.log(JSON.stringify(res));
+									}
+								})
+								
+							}
 						})
-						
-                       
-                         
+							that.uploadimg = res.tempFilePaths[0];
+							//触发所选择文件更改事件，参数为文件列表
+							that.$emit('change', that.uploadimg);
+					}
+				});
+				},
+			previewImage(){
+				//预览图片
+				uni.previewImage({
+					urls: [this.uploadimg]
+				});
+			},
+			removeImage(img) {
+				var that = this;
+				uni.showModal({
+					content: '确认删除选中的图片？',
+					success: function() {
+						 that.uploadimg = '';
 					}
 				});
 			},
-// 			removeImage(img) {
-// 				uni.showModal({
-// 					content: '确认删除选中的图片？',
-// 					success: function() {
-// 						 if (res.confirm) {
-// 									that.uploadimg = '';
-// 								} else if (res.cancel) {
-// 									
-// 								}
-// 						// const index = this.copyImages.findIndex(item => item === img);
-// 						// this.copyImages.splice(0, 1);
-// 						// console.log(t0his.copyImages)
-// 						
-// 					}
-// 				});
-// 			},
 			getTask(){
 				const jsonString = {
 					orderInfo:this.orderInfo,
@@ -154,7 +139,6 @@
 					success(res) {
 						console.log(res.data);
 						if(res.data.errorCode =='0000'){
-						    // 您已经提交任务,请等待管理员审核
 							uni.showModal({
 								content:'您已经提交任务,请等待管理员审核',
 								
